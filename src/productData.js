e164e5a43cffd7cd82a0811f9981c3264af0534c
Adam3192 <jeremyadixon@hotmail.com>
2022-06-20 12:11:48 -0500

    lesson 8 assignment - react

--- src/productData.js
+++ src/productData.js
@@ -5,26 +5,26 @@ module.exports = () => {
         id: 1,
         productName: 'Coffee',
         description: 'Bottomless House Roasted Coffee',
-        price: 2.5,
+        price: 2.50
       },
       {
         id: 2,
         productName: 'Soda',
         description: 'Choice of any fountain sparkling beverage',
-        price: 2.5,
+        price: 2.75
       },
       {
         id: 3,
         productName: 'Chocolate Milkshake',
         description:
           'Dark chocolate whipped with locally sourced milk and sweetened with sugar',
-        price: 2.5,
+        price: 2.25
       },
       {
         id: 4,
         productName: 'Pecan Pie',
         description: 'A slice of our exquisite Pecan Pie',
-        price: 2.5,
+        price: 3.25
       },
     ],
   }
